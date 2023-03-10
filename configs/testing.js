c1469221a68bc83c01bd6f248e661f56fb4e9804
chriszarate <chris.zarate@automattic.com>
2023-03-10 17:26:14 -0500

    Add jest plugin

--- configs/testing.js
+++ configs/testing.js
@@ -1,4 +1,7 @@
 module.exports = {
+	plugins: [
+		'jest',
+	],
 	/**
 	 * Please include a short description of the rule. For rules that downgrade or
 	 * disable errors, include a brief justification or reasoning.
