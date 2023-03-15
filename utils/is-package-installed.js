22a086ec08915fd444024a8bd5db0205bb62de0f
chriszarate <chris.zarate@automattic.com>
2023-03-15 16:24:13 -0400

    Simplify is-package-installed

--- utils/is-package-installed.js
+++ utils/is-package-installed.js
@@ -1,28 +1,8 @@
-const fs = require('node:fs');
 const debugLog = require('./debug-log');
 const findPackageJson = require('find-package-json');
 
-function findParent() {
-	const packages = [...findPackageJson()];
-	const firstPackage = packages.shift();
-
-	// A little hack to help us use this plugin to lint itself: When installed via
-	// NPM, .eslintrc.js will be missing (due to .npmignore).
-	if ('@automattic/eslint-plugin-wpvip' === firstPackage.name) {
-		const eslintRc = firstPackage.__path.replace(
-			/package\.json$/,
-			'.eslintrc.js'
-		);
-		// eslint-disable-next-line security/detect-non-literal-fs-filename
-		if (fs.statSync(eslintRc)) {
-			return firstPackage;
-		}
-	}
-
-	return packages.pop() || {};
-}
-
-const parent = findParent();
+const packages = [...findPackageJson(__dirname)];
+const parent = packages.pop() || {};
 
 debugLog(`Found package.json: ${parent.__path || 'none'}`);
 
