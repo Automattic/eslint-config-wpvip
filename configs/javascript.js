66582e336ded3151f652703e201a965b7bfe6074
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2026-07-29 00:55:07 +0300

    refactor: remove `@babel/eslint-parser`

--- configs/javascript.js
+++ configs/javascript.js
@@ -6,7 +6,6 @@
  * - https://github.com/WordPress/gutenberg/blob/%40wordpress/eslint-plugin%4014.1.0/packages/eslint-plugin/configs/recommended-with-formatting.js
  */
 
-const babelParser = require( '@babel/eslint-parser' );
 const ImportPlugin = require( 'eslint-plugin-import' );
 const JsonPlugin = require( 'eslint-plugin-json' );
 const PromisePlugin = require( 'eslint-plugin-promise' );
@@ -39,12 +38,6 @@ module.exports = [
 			globals: {
 				...globals.node,
 				...globals.nodeBuiltin,
-			},
-			parser: babelParser,
-			parserOptions: {
-				requireConfigFile: false,
-				sourceType: 'module',
-				ecmaVersion: 2022,
 			},
 		},
 		plugins: {
